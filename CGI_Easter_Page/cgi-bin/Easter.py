aa77136f5d0a288c2a5b243fdb0457538dffee1b
rhys909 <connorR@cardiff.ac.uk>
2018-12-05 17:16:44 +0000

    FIXED AND WORKING

--- CGI_Easter_Page/cgi-bin/Easter.py
+++ CGI_Easter_Page/cgi-bin/Easter.py
@@ -1,18 +1,44 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 import cgi, cgitb
 import datetime
 cgitb.enable()
 form = cgi.FieldStorage()
-y = form.getvalue('year')
+y = int(form.getvalue('year'))
 
 option = form.getvalue('Format')
 
 
-def Easter_Numeric():
-    date_of_easter_num = datetime.date(day=p) + '/' + datetime.date(month=n) + '/' + datetime.date(year=y)
-    return str(date_of_easter_num)
+def Easter_Numeric(y):
+    a = y % 19
+    b = y // 100
+    c = y % 100
+    d = b // 4
+    e = b % 4
+    g = (8 * b + 13) // 25
+    h = (19 * a + b - d - g + 15) % 30
+    j = c // 4
+    k = c % 4
+    m = (a + 11 * h) // 319
+    r = (2 * e + 2 * j - k - h + m + 32) % 7
+    n = (h - m + r + 90) // 25
+    p = (h - m + r + n + 19) % 32
+    date_of_easter_num = str(p) + '/' + str(n) + '/' + str(y)
+    print(date_of_easter_num)
 
-def Easter_String():
+def Easter_String(y):
+    a = y % 19
+    b = y // 100
+    c = y % 100
+    d = b // 4
+    e = b % 4
+    g = (8 * b + 13) // 25
+    h = (19 * a + b - d - g + 15) % 30
+    j = c // 4
+    k = c % 4
+    m = (a + 11 * h) // 319
+    r = (2 * e + 2 * j - k - h + m + 32) % 7
+    n = (h - m + r + 90) // 25
+    p = (h - m + r + n + 19) % 32
     if n == 3:
         n = 'March'
     else:
@@ -27,40 +53,26 @@ def Easter_String():
     else:
         sup = 'th'
 
-    date_of_easter_str = str(p) + sup + ' of ' + str(z) + ' ' + str(y) + '.'
-    return date_of_easter_str
+    print(str(p) + '<sup>' + sup + '</sup>' + ' of ' + str(n) + ' ' + str(y))
 
 print('Content-Type: text/html; charset=utf-8')
 print('')
 print('<!DOCTYPE html>')
 print('<html>')
-print('<head><title> Python Script</title></head>')
+print('<head> <meta charset="utf-8">')
+print('<title> Python Script</title>')
+print('<link rel="stylesheet" href="../style.css">')
+print('</head>')
 print('<body>')
 print('<p>')
-if y.isdigit() == True:
-y = int(y)
-a = y % 19
-b = y // 100
-c = y % 100
-d = b // 4
-e = b % 4
-g = (8 * b + 13) // 25
-h = (19 * a + b - d - g + 15) % 30
-j = c // 4
-k = c % 4
-m = (a + 11 * h) // 319
-r = (2 * e + 2 * j - k - h + m + 32) % 7
-n = (h - m + r + 90) // 25
-p = (h - m + r + n + 19) % 32
-else:
-    print('<h2> This is not a valid year.</h2>')
 if option == 'numeric':
-    date_of_easter = date_of_easter_num
+    Easter_Numeric(y)
 elif option == 'string':
-    date_of_easter = date_of_easter_str
+    Easter_String(y)
 else:
-    date_of_easter = Easter_String() + ' ' + Easter_Numeric()
-print('Easter in the year' + str(y) + 'will fall on' + date_of_easter + '.<br><br>')
+    Easter_Numeric(y)
+    print('<br>')
+    Easter_String(y)
 print('</p>')
 print('</body>')
 print('</html>')
