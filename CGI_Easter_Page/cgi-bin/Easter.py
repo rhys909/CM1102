6830401198331efa45287975a2c1f64f3c9b455b
rhys909 <connorR@cardiff.ac.uk>
2018-12-05 00:29:27 +0000

    still getting errors

--- CGI_Easter_Page/cgi-bin/Easter.py
+++ CGI_Easter_Page/cgi-bin/Easter.py
@@ -4,23 +4,7 @@ import datetime
 cgitb.enable()
 form = cgi.FieldStorage()
 y = form.getvalue('year')
-if y.isalpha() == True:
-y = int(y)
-a = y % 19
-b = y // 100
-c = y % 100
-d = b // 4
-e = b % 4
-g = (8 * b + 13) // 25
-h = (19 * a + b - d - g + 15) % 30
-j = c // 4
-k = c % 4
-m = (a + 11 * h) // 319
-r = (2 * e + 2 * j - k - h + m + 32) % 7
-n = (h - m + r + 90) // 25
-p = (h - m + r + n + 19) % 32
-else:
-    print('<h2> This is not a valid year.</h2>')
+
 option = form.getvalue('Format')
 
 
@@ -44,7 +28,7 @@ def Easter_String():
         sup = 'th'
 
     date_of_easter_str = str(p) + sup + ' of ' + str(z) + ' ' + str(y) + '.'
-
+    return date_of_easter_str
 
 print('Content-Type: text/html; charset=utf-8')
 print('')
@@ -53,10 +37,27 @@ print('<html>')
 print('<head><title> Python Script</title></head>')
 print('<body>')
 print('<p>')
+if y.isdigit() == True:
+y = int(y)
+a = y % 19
+b = y // 100
+c = y % 100
+d = b // 4
+e = b % 4
+g = (8 * b + 13) // 25
+h = (19 * a + b - d - g + 15) % 30
+j = c // 4
+k = c % 4
+m = (a + 11 * h) // 319
+r = (2 * e + 2 * j - k - h + m + 32) % 7
+n = (h - m + r + 90) // 25
+p = (h - m + r + n + 19) % 32
+else:
+    print('<h2> This is not a valid year.</h2>')
 if option == 'numeric':
-    date_of_easter = Easter_Numeric()
+    date_of_easter = date_of_easter_num
 elif option == 'string':
-    date_of_easter = Easter_String()
+    date_of_easter = date_of_easter_str
 else:
     date_of_easter = Easter_String() + ' ' + Easter_Numeric()
 print('Easter in the year' + str(y) + 'will fall on' + date_of_easter + '.<br><br>')
