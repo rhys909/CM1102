909f7fadfe35e8e4edb46daa1b6dd582647df060
Rhys909 <rhyskconnor99@gmail.com>
2019-11-15 10:58:51 +0000

    Eh

--- Coursework_Shop/Shop/__init__.py
+++ Coursework_Shop/Shop/__init__.py
@@ -4,7 +4,7 @@ from flask_login import LoginManager
 
 app = Flask(__name__, static_folder="Static")
 app.config['SECRET_KEY'] = 'dd8382eff8d9254f376806101a3e7bebd68b0b21a80178b0'
-app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://c1821631:H0tr0d99@csmysql.cs.cf.ac.uk:3306/c1821631'
+app.config['SQLALCHEMY_DATABASE_URI']= 'mysql+pymysql://c1821631:Password@csmysql.cs.cf.ac.uk:3306/c1821631'
 
 db = SQLAlchemy(app)
 login_manager = LoginManager()
